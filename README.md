56d28bf23ba2603abe4bd753d3a8afa91b455d42
ghUserMan <apatiy.kirov@yandex.ru>
2022-02-26 18:56:04 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## 17 Новое приложение - чат в релаьном времени
+
+По прежнему важное место для тестов 
+https://jsbin.com/bopisujaki/edit?html,output
+https://docs.mongodb.com/drivers/node/current/usage-examples/find/
+https://nodejs.org/dist/latest-v16.x/docs/api/path.html
+http://expressjs.com/en/4x/api.html
+
+### 17.2 Creating the Chat App Project
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT | 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.json()) 
+app.use(express.static(publicDirectoryPath))
+
+app.get('/hi', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port', port)
+})
