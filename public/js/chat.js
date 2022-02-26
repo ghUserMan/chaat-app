502fb5788ac009160c34d60773e6648ef7d68f12
ghUserMan <apatiy.kirov@yandex.ru>
2022-02-26 22:12:50 +0300

    17.6

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,14 +6,29 @@ const socket = io() // сохраняем открытый сокет
 
 // получаем от севрера 
 // count доступен ведь мы его пеердаём специально
-socket.on('countUpdated', (count) => {
-    console.log('Count has been updated', count)
+// socket.on('countUpdated', (count) => {
+//     console.log('Count has been updated', count)
+// })
+
+
+// const increment = document.querySelector('#increment')
+
+// increment.addEventListener('click', () => {
+//     console.log('Clocked!')
+//     socket.emit('increment')
+// })
+
+socket.on('message', (message) => {
+    console.log('from server', message)
 })
 
+// пожно повесить слушателя не на кнопку, а на форму!
+// const sendMessage = document.querySelector('#message-form') ну и значение получать чеерз event 
+// event.targer.elements.message.value (где  message - имя поля дял ввода)
+const sendMessage = document.querySelector('#sendMessage') 
 
-const increment = document.querySelector('#increment')
-
-increment.addEventListener('click', () => {
-    console.log('Clocked!')
-    socket.emit('increment')
+sendMessage.addEventListener('click', (event) => {
+    event.preventDefault()
+    const text = document.querySelector('#message').value
+    socket.emit('sendMessage', text)
 })
--- src/index.js
+++ src/index.js
@@ -16,8 +16,7 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.json()) 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
+// let count = 0
 
 app.get('/hi', (req, res) => {
     res.send('hi')
@@ -32,17 +31,23 @@ app.get('/hi', (req, res) => {
 io.on('connection', (socket) => {
     console.log('new websocket connection')
 
-    // указываем какое событие посылается в зад, это наше личное сообщение
-    // всё что после имени события - будет доступно на клиенте
-    socket.emit('countUpdated', count)
+    // // указываем какое событие посылается в зад, это наше личное сообщение
+    // // всё что после имени события - будет доступно на клиенте
+    // socket.emit('countUpdated', count)
 
-    socket.on('increment', () => {
-        console.log('Server inc')
-        count++
-        // socket.emit('countUpdated', count) // работает только для одного (текущего) соединения
-        io.emit('countUpdated', count) // для всех соединений (а я думал в список придётся складывать) 
-    })
+    // socket.on('increment', () => {
+    //     console.log('Server inc')
+    //     count++
+    //     // socket.emit('countUpdated', count) // работает только для одного (текущего) соединения
+    //     io.emit('countUpdated', count) // для всех соединений (а я думал в список придётся складывать) 
+    // })
     
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (text) => {
+        console.log('from client', text)
+        io.emit('message', text)
+    })
 })
 
 server.listen(port, () => {
